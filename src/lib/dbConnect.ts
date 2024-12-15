a3586b141091c3b5463fe81b73cb816f3aae2e0f
AYUSH KUMAR GUPTA <ayushkumargupta2908@gmail.com>
2024-12-15 17:56:59 +0530

    Database connection

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+type ConnectionObject = {
+    isConnected?: number;
+}
+
+const connection: ConnectionObject = {};
+
+
+async function dbConnect():Promise<void> {
+    if(connection.isConnected){
+        console.log("Already connected to database");
+        return;
+    }
+    try {
+        const dbConnection = await mongoose.connect(process.env.MONGODB_URI || "", {})
+        connection.isConnected = dbConnection.connections[0].readyState;
+        
+        console.log("Ready  state: ", connection.isConnected);
+        console.log("Connected to database Successfully");
+    } catch (error) {
+        console.log("Error connecting to database: ", error);
+        process.exit(1); //graacefully exit the process
+    }
+}
+
+dbConnect();
+
+export default dbConnect
