f91c622082520607d9ff2b996653bcca2bb88530
AYUSH KUMAR GUPTA <ayushkumargupta2908@gmail.com>
2024-12-16 21:49:09 +0530

    Enhance middleware to redirect unauthenticated users from dashboard to sign-in page

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { NextResponse,NextRequest } from 'next/server'
 // import type { NextRequest } from 'next/server'
-export { default } from "next-auth/middleware"
+export { default } from "next-auth/middleware" //apply on all routes
 import { getToken } from "next-auth/jwt"
 
 
@@ -22,6 +22,9 @@ export async function middleware(request: NextRequest) {
         return NextResponse.redirect(new URL('/dashboard', request.url))
     }
 
+    if(!token && url.pathname.startsWith('/dashboard')){
+        return NextResponse.redirect(new URL('/sign-in', request.url))
+    }
     return NextResponse.next()
 }
 
