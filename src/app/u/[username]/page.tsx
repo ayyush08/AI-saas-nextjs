d33a3141618b046d045daa4cf53964e456acb78d
AYUSH KUMAR GUPTA <ayushkumargupta2908@gmail.com>
2024-12-21 20:22:10 +0530

    Enhancements, user friendly additions, finalizations

--- src/app/u/[username]/page.tsx
+++ src/app/u/[username]/page.tsx
@@ -13,21 +13,24 @@ import * as z from 'zod'
 import axios, { AxiosError } from 'axios'
 import { ApiResponse } from '@/types/ApiResponse'
 import { useToast } from '@/hooks/use-toast'
+import { Card, CardContent, CardHeader } from '@/components/ui/card'
+import { Separator } from '@/components/ui/separator'
+import Link from 'next/link'
 
 const initialSuggestions = 'What is your favorite color? || What is your favorite food? || What is your favorite movie?'
+
+const parseStringMessages = (messages:string):string[]=>{
+  console.log(messages);
+  
+  return messages.split('||')
+}
 
 const page = () => {
   const [isLoading,setIsLoading] = useState(false)
   const {toast} = useToast()
-  const {
-    complete,
-    completion,
-    isLoading: isSuggestionLoading,
-    error
-  } = useCompletion({
-    api:'/api/suggest-messages',
-    initialCompletion: initialSuggestions
-  })
+  const [messages,setMessages] = useState(initialSuggestions);
+  const [isSuggestionLoading,setIsSuggestionLoading] = useState(false)
+  const [suggestionError,setSuggestionError] = useState('')
   const params = useParams();
   const username = params.username
   
@@ -48,6 +51,7 @@ const page = () => {
         title:response.data.message,
         variant:'default'
       })
+      form.reset({ ...form.getValues(), content: '' });
     } catch (error) {
       const axiosError = error as AxiosError<ApiResponse>
       toast({
@@ -60,10 +64,45 @@ const page = () => {
       setIsLoading(false)
     }
   }
+
+
+  const fetchSuggestedMessages = async()=>{
+    try {
+      setIsSuggestionLoading(true)
+      const messages = await axios.post('/api/suggest-messages')
+      setMessages(messages.data.messages)
+      toast({
+        title:"New messages suggested",
+        variant:'default'
+      })
+      
+    } catch (error) {
+      console.log(error);
+      const axiosError = error as AxiosError<ApiResponse>
+      setSuggestionError(axiosError.response?.data?.message || 'Something went wrong')
+      toast({
+        title:"Error suggesting messages",
+        description: axiosError.response?.data?.message || "Something went wrong",
+        variant:'destructive'
+      })
+    }finally{
+      setIsSuggestionLoading(false)
+    }
+  }
+
+  const handleMessageClick = (message:string)=>{
+    form.setValue('content',  message)
+  }
+
   return (
+    <>
+    <div className='mx-auto p-5 text-center'>
+      <a href="/" className='text-4xl  font-bold mb-4 md:mb-0'>Anonymous Message</a>
+
+    </div>
     <div className="container mx-auto my-8 p-6 bg-white rounded max-w-4xl">
-      <h1 className="text-4xl font-bold mb-6 text-center">
-        Public Profile Link
+      <h1 className="text-2xl font-bold mb-6 text-center">
+        Public Profile of @{username}
       </h1>
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
@@ -83,7 +122,7 @@ const page = () => {
                 <FormMessage />
               </FormItem>
             )}
-          />
+            />
           <div className="flex justify-center">
             {isLoading ? (
               <Button disabled>
@@ -98,7 +137,49 @@ const page = () => {
           </div>
         </form>
       </Form>
+      <div className="space-y-4 my-8">
+        <div className="space-y-2">
+          <Button
+            onClick={fetchSuggestedMessages}
+            className="my-4"
+            disabled={isSuggestionLoading}
+          >
+            Suggest Messages
+          </Button>
+          <p>Click on any message below to select it.</p>
+        </div>
+        <Card>
+          <CardHeader>
+            <h3 className="text-xl font-semibold">Messages</h3>
+          </CardHeader>
+          <CardContent className={`flex flex-col space-y-4 ${isSuggestionLoading ? 'opacity-20 animate-pulse ' : ''} transition-opacity duration-600`}>
+            {suggestionError ? (
+              <p className="text-red-500">{suggestionError}</p>
+            ) : (
+              parseStringMessages(messages).map((message, index) => (
+                <Button
+                key={index}
+                variant="outline"
+                className="mb-2"
+                data-testid="generation"
+                onClick={() => handleMessageClick(message)}
+                >
+                  {message}
+                </Button>
+              ))
+            )}
+          </CardContent>
+        </Card>
+      </div>
+      <Separator className="my-6" />
+      <div className="text-center">
+        <div className="mb-4">Get Your Message Board</div>
+        <Link href={'/sign-up'}>
+          <Button>Create Your Account</Button>
+        </Link>
+      </div>
     </div>
+            </>
   )
 }
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,13 +4,91 @@ import Link from 'next/link'
 import { useSession, signOut } from 'next-auth/react'
 import { User } from 'next-auth'
 import { Button } from './ui/button'
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+import { Form, FormControl, FormField, FormItem, FormLabel } from './ui/form'
+import { useForm } from 'react-hook-form'
+import { useRouter } from 'next/navigation'
+import { Textarea } from './ui/textarea'
+import { useToast } from '@/hooks/use-toast'
 
+interface AskLinkProps {
+    url: string
+}
+
+const AskLink = () => {
+    const { toast } = useToast()
+    const form = useForm<AskLinkProps>({
+        defaultValues: { url: '' }
+    });
+    const router = useRouter()
+    const onSubmit = (data: AskLinkProps) => {
+        try {
+            new URL(data.url)
+            router.replace(data.url)
+
+        } catch (error) {
+            toast({
+                title: "Invalid Url",
+                variant: 'destructive'
+            })
+        }
+    }
+    return (
+        <AlertDialog>
+            <AlertDialogTrigger asChild>
+                <Button variant='outline'>Send Message</Button>
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+                <AlertDialogHeader>
+                    <AlertDialogTitle>Send Message</AlertDialogTitle>
+                    <AlertDialogDescription>
+                        Enter the receiver's public profile url to send them an anonymous message
+                    </AlertDialogDescription>
+                </AlertDialogHeader>
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+                        <FormField
+                            control={form.control}
+                            name="url"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormControl>
+                                        <Textarea
+                                            placeholder="Paste public profile url"
+                                            className="resize-none"
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                </FormItem>
+                            )}
+                        />
+                        <AlertDialogFooter>
+                            <AlertDialogCancel>Cancel</AlertDialogCancel>
+                            <AlertDialogAction type="submit">Go to Url</AlertDialogAction>
+                        </AlertDialogFooter>
+                    </form>
+                </Form>
+            </AlertDialogContent>
+        </AlertDialog>
+
+    )
+}
 
 const Navbar = () => {
     const { data: session } = useSession()
     const user: User = session?.user as User //assertion
     console.log(window.location);
-    
+
     return (
         <nav className='p-4 md:p-6 shadow-md'>
             <div className='container mx-auto flex flex-col md:flex-row justify-between items-center'>
@@ -19,21 +97,20 @@ const Navbar = () => {
                     session ? (<>
                         <span className=' mx-auto font-bold text-2xl'>Welcome, {user.username || user.email}</span>
                         <div className='flex flex-col md:flex-row gap-4'>
-                        {window.location.pathname!=='/dashboard' &&
-                            <Link href='/dashboard'>
-                            <Button variant='outline' className='w-full md:w-auto'>Dashboard</Button>
-                        </Link>}
-                        <Link href={`/u/${user.username}`}>
-                            <Button variant='outline' className='w-full md:w-auto'>Send Message</Button>
-                        </Link>
-                        <Button className='w-full md:w-auto' onClick={()=>signOut()} >Logout</Button>
+                            {window.location.pathname !== '/dashboard' &&
+                                <Link href='/dashboard'>
+                                    <Button variant='outline' className='w-full md:w-auto'>Dashboard</Button>
+                                </Link>}
+                            <AskLink />
+
+                            <Button className='w-full md:w-auto' onClick={() => signOut()} >Logout</Button>
                         </div>
-                        </>
+                    </>
                     ) : (
                         <Link href='/sign-in'>
                             <Button className='w-full md:w-auto'>Login</Button>
                         </Link>
-                        
+
                     )
                 }
             </div>
